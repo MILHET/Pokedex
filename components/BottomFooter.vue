3781656d65ccb0e7a9d118b9019a5ccb7209ae0e
françois <francoismilhet@hotmail.fr>
2020-05-16 16:18:54 +0100

    Add core Components + Main Page

---
+++ components/BottomFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="bottom-footer">
+
+    <div class="util__container">
+      François MILHET ©copyright
+    </div>
+
+  </footer>
+</template>
+
+<script>
+    export default {
+        name: "BottomFooter"
+    }
+</script>
+
+<style scoped lang="scss">
+  .bottom-footer {
+    background: black;
+    padding: 40px 0 40px 0;
+    text-align: center;
+    color: white;
+
+    &__link {
+      color: white;
+      text-decoration: none;
+    }
+  }
+</style>
---
+++ components/TopHeader.vue
@@ -0,0 +1,91 @@
+<template>
+  <header class="top-header util__flex util__container">
+
+    <nav class="top-header__col">
+
+      <ul class="nav">
+        <li>
+          <nuxt-link class="nav__item" to="/">Home</nuxt-link>
+        </li>
+      </ul>
+
+    </nav>
+
+    <nuxt-link to="/" class="top-header__col top-header__logo">
+      <img src="../assets/img/pokeball.png">
+    </nuxt-link>
+
+  </header>
+</template>
+
+<script>
+    export default {
+        name: "TopHeader"
+    }
+</script>
+
+<style scoped lang="scss">
+  .top-header {
+    justify-content: space-between;
+    padding-top: 30px;
+    padding-bottom: 30px;
+
+    .nav__item {
+      padding: 5px;
+      color: white;
+      text-decoration: none;
+
+      &:hover {
+        color: yellow;
+      }
+    }
+
+    .nav__item__right {
+      position: absolute;
+      right: 5%;
+      background-color: yellow;
+      color: black;
+
+      &:hover {
+        background-color: orangered;
+      }
+    }
+
+    &__logo {
+      text-align: center;
+      position: absolute;
+      left: 50%;
+
+      img {
+        position: relative;
+        max-height: 60px;
+        left: -50%;
+        top: -15px;
+      }
+    }
+  }
+
+  .sidebar {
+    text-align: right;
+  }
+
+  .img-thumbnail {
+    border: none;
+    background: none;
+  }
+
+  .btn-danger {
+    margin-left: 5px;
+    margin-right: 5px;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+
+    li {
+      cursor: pointer;
+    }
+  }
+
+</style>
